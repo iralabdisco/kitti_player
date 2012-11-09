08f1e6d7e31e67a101cfa6ae155f1ccad96f7a90
Francesco Sacchi <depsir@gmail.com>
2012-11-09 18:37:30 +0100

    Seems ok.. transformation and laser and frames

--- src/kitti_player.cpp
+++ src/kitti_player.cpp
@@ -25,81 +25,92 @@ btTransform pose;
 std::ifstream* poseFile = NULL;
 
 std::string laser_frame;
+std::string camera_ref_frame;
+std::string camera_ref_zero_frame;
 std::string robot_frame;
-std::string world_frame;
+std::string odom_frame;
 
 void callback(kitti_player::kitti_playerConfig &config, uint32_t level)
 {
+
   if (config.sequence != myConfig.sequence || config.start == false)
   {
     frame_count = 0;
-    std::stringstream ss;
-    ss << std::setfill('0') << std::setw(2) << config.sequence; 
-   
-    ss >> sequence;
-
-    pose_path = path+"/../dataset/poses/"+sequence+".txt";
     delete poseFile;
     poseFile = NULL;
     
   }
-  if (poseFile == NULL)
-  {
-    poseFile = new std::ifstream(pose_path.c_str());
-  }
+    
   myConfig = config;
-
 
   if (config.publish)
   {
     config.publish = false;
   }
-
 }
 
 void publish_pose()
 {
-  if (poseFile)
+  if (!poseFile)
   {
-    if( !poseFile->good()){
-      std::cerr << "Could not read file: " << *poseFile << std::endl;
-    }
-    else
-    {
-      static tf::TransformBroadcaster br;
-      tf::Transform transform;
-      std::string line;
-      getline(*poseFile, line);
-
-      std::cout << line << std::endl;
-
-      std::vector<std::string> strs;
-      boost::split(strs, line, boost::is_any_of("\t "));
-      std::vector<double> values;
-
-      for ( std::vector<std::string>::iterator it=strs.begin() ; it < strs.end(); it++ )
-      {
-        double numb;
-        std::istringstream ( *it ) >> numb;
-        values.push_back(numb);
-        std::cout << numb <<std::endl;
-      }
-
-
-
-      transform.setOrigin( tf::Vector3(values[3], values[7], values[11]) );
-      
-      transform.setBasis( btMatrix3x3(values[0], values[1], values[2],values[4], values[5], values[6],values[8], values[9], values[10]) );
-      br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), robot_frame, world_frame));
-      transform.setIdentity();
-      br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), robot_frame, laser_frame));
-
-    }
+  std::cout << "new" << pose_path << std::endl;
+    poseFile = new std::ifstream(pose_path.c_str());
+  
+  }
+  if( !poseFile->good()){
+    std::cerr << "Could not read file: " << *poseFile << std::endl;
   }
   else
   {
-    std::cerr << "No file"  << std::endl;
+    static tf::TransformBroadcaster br;
+    tf::Transform transform;
+    std::string line;
+    getline(*poseFile, line);
+
+    std::cout << line << std::endl;
+
+    std::vector<std::string> strs;
+    boost::split(strs, line, boost::is_any_of("\t "));
+    std::vector<double> values;
+
+    for ( std::vector<std::string>::iterator it=strs.begin() ; it < strs.end(); it++ )
+    {
+      double numb;
+      std::istringstream ( *it ) >> numb;
+      values.push_back(numb);
+      std::cout << numb <<std::endl;
+    }
+
+
+
+    transform.setOrigin( tf::Vector3(values[3], values[7], values[11]) );
+    
+    transform.setBasis( btMatrix3x3(values[0], values[1], values[2],values[4], values[5], values[6],values[8], values[9], values[10]) );
+    // transform.setIdentity();
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), camera_ref_zero_frame, camera_ref_frame));
+    
+    transform.setIdentity();
+    transform.setOrigin(btVector3(0.76,0,1.73));
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), robot_frame, laser_frame));
+
+    transform.setIdentity();
+    transform.setOrigin(btVector3(1.03,0,1.65));
+    btQuaternion rotation;
+
+    btQuaternion rot1;
+    btQuaternion rot2;
+
+    rot1.setEuler(M_PI/2,0,0);
+    rot2.setEuler(0,-M_PI/2,0);
+    // rotation = rot1;
+    rotation = rot2*rot1;
+    
+    transform.setRotation(rotation);
+    br.sendTransform(tf::StampedTransform(transform, ros::Time::now(), odom_frame, camera_ref_zero_frame));
+    br.sendTransform(tf::StampedTransform(transform.inverse(), ros::Time::now(), camera_ref_frame, robot_frame));
+
   }
+  
 }
 
 void publish_velodyne(ros::Publisher &pub, std::string infile)
@@ -148,13 +159,15 @@ int main(int argc, char **argv)
   std::string::size_type pos = path.find_last_of( "\\/" );
   path = path.substr( 0, pos);
 
+  n.param<std::string>("camera_ref_frame",camera_ref_frame,"camera_ref");
+  n.param<std::string>("camera_ref_zero_frame",camera_ref_zero_frame,"camera_ref_zero");
   n.param<std::string>("laser_frame",laser_frame,"laser");
   n.param<std::string>("robot_frame",robot_frame,"base_link");
-  n.param<std::string>("world_frame",world_frame,"map");
+  n.param<std::string>("odom_frame",odom_frame,"odom");
 
 
-  sequence_path = path+"/../dataset/sequences/01/velodyne/0000000000.bin";
-  pose_path = path+"/../dataset/poses/01.txt";
+  // sequence_path = path+"/../dataset/sequences/01/velodyne/0000000000.bin";
+  // pose_path = path+"/../dataset/poses/01.txt";
 
   
 
@@ -162,8 +175,14 @@ int main(int argc, char **argv)
   {
     if (myConfig.start && (myConfig.continuous || myConfig.publish))
     {
+
       std::stringstream ss;
-        
+      ss << std::setfill('0') << std::setw(2) << myConfig.sequence; 
+     
+      ss >> sequence;
+      pose_path = path+"/../dataset/poses/"+sequence+".txt";
+              
+      ss.clear();
       // the number is converted to string with the help of stringstream
       ss << std::setfill('0') << std::setw(6) << frame_count; 
       std::string frame;
