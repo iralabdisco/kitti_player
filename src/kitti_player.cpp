ff233463b9becb192519f29499729c59f06fab61
Tomas Krejci <tomas789@gmail.com>
2016-11-27 19:02:41 +0100

    Ignoring some parameters

--- src/kitti_player.cpp
+++ src/kitti_player.cpp
@@ -486,7 +486,7 @@ int main(int argc, char **argv)
 
     try // parse options
     {
-        po::parsed_options parsed = po::command_line_parser(argc - 2, argv).options(desc).allow_unregistered().run();
+        po::parsed_options parsed = po::command_line_parser(argc, argv).options(desc).allow_unregistered().run();
         po::store(parsed, vm);
         po::notify(vm);
 
